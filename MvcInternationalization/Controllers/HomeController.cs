f12cf03cf1069a77fe8effd01e0eaee2f9db01c1
YK Poh <yk@mysoftinn.com>
2017-03-12 10:47:31 +0800

    add post endpoint and set user language from cookies

--- MvcInternationalization/Controllers/HomeController.cs
+++ MvcInternationalization/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcInternationalization.Helpers;
+using MvcInternationalization.Models;
 
 namespace MvcInternationalization.Controllers
 {
@@ -11,6 +13,29 @@ namespace MvcInternationalization.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person per)
+        {
+            return View();
+        }
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
